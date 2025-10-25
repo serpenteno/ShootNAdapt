b03c2ddd74f2a3dacf688e74a90083f870c40e4c
serpenteno <serpenteno.dev@gmail.com>
2025-10-25 16:14:05 +0200

    Add cooperation with server (with heuristics)

---
+++ Server/main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from enum import IntEnum
+
+
+app = FastAPI(title="ShootNAdapt")
+
+
+class PlayerStats(BaseModel):
+    accuracy: float
+    reactionTimeRatio: float
+    
+
+class DifficultyLevel(IntEnum):
+    EASY = 0
+    NORMAL = 1
+    HARD = 2
+   
+    
+class DifficultyLevels(BaseModel):
+    targetSize: DifficultyLevel
+    targetLifeSpan: DifficultyLevel
+    
+    def __init__(self, targetSize: DifficultyLevel, targetLifeSpan: DifficultyLevel) -> None:
+        super().__init__(targetSize=targetSize, targetLifeSpan=targetLifeSpan)
+        self.targetSize = targetSize
+        self.targetLifeSpan = targetLifeSpan
+
+
+@app.post("/send_player_stats")
+def send_player_stats(player_stats: PlayerStats) -> DifficultyLevels:
+    targetSize: DifficultyLevel
+    targetLifeSpan: DifficultyLevel
+    
+    if player_stats.accuracy < 0.5:
+        targetSize = DifficultyLevel.EASY
+    elif player_stats.accuracy > 0.9:
+        targetSize = DifficultyLevel.HARD
+    else:
+        targetSize = DifficultyLevel.NORMAL
+        
+        
+    if player_stats.reactionTimeRatio < 0.2:
+        targetLifeSpan = DifficultyLevel.HARD
+    elif player_stats.reactionTimeRatio > 0.8:
+        targetLifeSpan = DifficultyLevel.EASY
+    else:
+        targetLifeSpan = DifficultyLevel.NORMAL
+    
+    
+    difficulty_levels = DifficultyLevels(
+        targetSize,
+        targetLifeSpan
+    )
+    return difficulty_levels
+
